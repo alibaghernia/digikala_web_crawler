78584f8bc439e66e7bdd7455929e1b97468adf86
alibaghernia <alibaghernia1379@gmail.com>
2021-10-09 03:07:46 +0330

    refactor code

--- digikala_web_crawler/spiders/product.py
+++ digikala_web_crawler/spiders/product.py
@@ -6,12 +6,9 @@ import random
 
 def normalize_str(text) -> str:
     if text is None:
-        text = ''
-    text = str(text)
-    text = text.strip()
-    text = text.replace('\n', '')
-    text = text.replace('  ', '')
-    return text
+        return ''
+
+    return str(text).strip().replace('\n', '').replace('  ', '')
 
 
 class Product(scrapy.Spider):
@@ -35,15 +32,20 @@ class Product(scrapy.Spider):
         product = dict()
         product['url'] = r.url
         product['title'] = normalize_str(r.css('div#content h1::text').get())
-        product['sub_title'] = normalize_str(r.css('div#content div.c-product__config > span::text').get())
-        product['description'] = normalize_str(r.css('div#desc div > div::text').get())
-        attribute_keys = r.css('div#params li > div.c-params__list-key').getall()
-        attribute_values = r.css('div#params li > div.c-params__list-value').getall()
+        product['sub_title'] = normalize_str(
+            r.css('div#content div.c-product__config > span::text').get())
+        product['description'] = normalize_str(
+            r.css('div#desc div > div::text').get())
+        attribute_keys = r.css(
+            'div#params li > div.c-params__list-key').getall()
+        attribute_values = r.css(
+            'div#params li > div.c-params__list-value').getall()
         attributes_tmp = list(zip(attribute_keys, attribute_values))
         attributes = []
         for index, (attr_key, attr_val) in enumerate(attributes_tmp):
             key = Selector(text=attr_key).css('div > span::text').get()
-            val = normalize_str(Selector(text=attr_val).css('div > span::text').get())
+            val = normalize_str(
+                Selector(text=attr_val).css('div > span::text').get())
             if key is None:
                 attributes[-1]['val'] += '\n' + val
             else:
@@ -78,4 +80,3 @@ class Product(scrapy.Spider):
         product['images'] = images
 
         yield product
-
